d4e658ef1aff7823235bdec45b8340972be37eec
NQDtotty <anhthoainguyen0907872008@gmail.com>
2023-04-01 21:49:05 +0700

    Update DDT to see red alert

--- test/com/nqdtotty/mathutil/core/MathUtilDDTTest.java
+++ test/com/nqdtotty/mathutil/core/MathUtilDDTTest.java
@@ -26,7 +26,7 @@ public class MathUtilDDTTest {
                             {3, 6},
                             {4, 24},
                             {5, 120},
-                            {6, 720}
+                            {6, 7200}
                           };      
     } 
     
